6bc40c618d8216b62cca5edf09c85128d50b36a5
Andrew VanNess <avanness@student.bridgew.edu>
2018-04-17 14:47:48 -0400

    added create or retrieve function

--- src/gameapp/user/views.py
+++ src/gameapp/user/views.py
@@ -5,7 +5,30 @@ import json
 from user.models import User
 from django.views.decorators.csrf import csrf_exempt
 
+
+
 class HomePageView(APIView):
+
+    @csrf_exempt
+    def create_or_retrieve(self, request=None, uname="test", format=None):
+        if request.method == 'GET':
+
+            found_user = User.objects.get(name=uname)
+            if(found_user):
+                return HttpResponse(json.dumps(data), status=200)
+            else:
+                return HttpResponse(json.dumps({"status":"NoSuchUser"}), status=404)
+
+        elif request.method == "POST":
+            try:
+                found_user = User.objects.get(name=uname)
+                return HttpResponse(json.dumps({"status":"AlreadyExists"}), status=403)
+            except ObjectDoesNotExist as e:
+                pass
+            u = User(name=uname)
+            u.save()
+            return HttpResponse(json.dumps({"status":"Success"}))
+
     def get(self, request=None, uname="test", format=None):
         try:
             found_user = User.objects.get(name=uname)
